43846a3ae1accc9d4d01ff6188e57b733f9143dc
chris <great.purpose@outlook.com>
2024-03-14 11:12:00 -0700

    integrate with grapejs

--- components/builder/index.vue
+++ components/builder/index.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
 import { useGrapes } from 'vue-grapesjs-composables';
-import 'vue-grapesjs-composables/css/vue-grapes.css';
+import 'grapesjs/dist/css/grapes.min.css';
 
-import divBlock from './plugins/div';
+import divBlock from './plugins';
 
 const canvas = ref(null)
 
 const grapes = useGrapes({
   container: canvas,
   height: 'auto',
-  plugins: [divBlock],
-  panels: {},
+  panels: {},  
 })
+
+grapes.onInit(divBlock)
 </script>
 
 <template>
-  <div class="ca-builder flex w-full min-h-full">
-    <BuilderBlocks :grapes="grapes" />
+  <div class="ca-builder flex w-full min-h-full">  
     <div
       ref="canvas"
       class="flex-1 min-h-full"
     />
   </div>
 </template>
+
+<style scoped >
+.gjs-devices-c {
+  padding: 2px 3px 0 3px;
+}
+
+</style>
