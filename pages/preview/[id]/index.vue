f8965d8aa84fd42040676aea72997a8db92e26b4
chris <great.purpose@outlook.com>
2024-04-10 16:17:51 -0700

    adding preview page

---
+++ pages/preview/[id]/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const route = useRoute()
+const { builder } = await usePreviewBuilder(route.params.id);  
+useHead({
+    script: builder.js,
+    style: builder.css,        
+})  
+
+const htmlString = ref(builder.html);
+
+</script>
+
+<template>
+   <div v-html="htmlString"></div>
+</template>
