245c4c4064c84745ca14cace7003d8cf20ec6f7d
chris <great.purpose@outlook.com>
2024-04-30 17:12:09 -0700

    update publish button

--- components/builder/plugins/devices/panels.ts
+++ components/builder/plugins/devices/panels.ts
@@ -107,8 +107,8 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
         },
         {
           id: cmdPublish,
-          command: () => editor.runCommand(cmdPublish),
-          label: 'Publish',          
+          command: () => editor.runCommand(cmdPublish),          
+          label: `<div class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 pt-1 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2">${exportedSVG['publishButton']}Publish</div>`,          
         }
       ]
     },
